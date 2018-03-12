1089bd1f44f23199a47b02084ff0b1c3dac628fb
Nayana <nayana@ieiworld.com>
2018-03-12 11:55:25 +0800

    Working Skeleton Example.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 // main.jsx  : Entry point of application react-skeleton
 var React = require('react');
 var ReactDOM = require('react-dom');
-var List = require('./components/List');
+var List = require('./components/List.jsx');
 
-ReactDOM.render(<List />, document.getElementById(ingredients));
+ReactDOM.render(<List />, document.getElementById('ingredients'));
